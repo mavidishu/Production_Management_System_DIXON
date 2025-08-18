967d47f5d308d596a8b453303e3087e8a2b75309
Dishu Mavi <dishumavi1@gmail.com>
2025-08-19 02:27:12 +0530

    feat : Added Lint Config File

--- src/components/EditRoute/EditProduct.jsx
+++ src/components/EditRoute/EditProduct.jsx
@@ -33,45 +33,45 @@ function EditProduct() {
     <div className="container productContainer mb-3" style={{marginTop:"5rem"}}>
         <h3 className='productTitle'>Update production</h3>
         <form method="POST" action={`http://localhost:5000/products/${product._id}?_method=PUT`} className="" >
-              <div class="row">
-                <div class=" mb-3">
-                  <label htmlFor="target" class="form-label mt-3" >Product Name</label>
-                  <input type="text" class="form-control" id="target" placeholder="" aria-label="Target For the Day" name='productName' value={product.productName} onChange={handleChange}/>
+              <div className="row">
+                <div className=" mb-3">
+                  <label htmlFor="target" className="form-label mt-3" >Product Name</label>
+                  <input type="text" className="form-control" id="target" placeholder="" aria-label="Target For the Day" name='productName' value={product.productName} onChange={handleChange}/>
                 </div>
-                <div class="">
-                  <label htmlFor="target" class="form-label" >Product Price</label>
-                  <input type="number" class="form-control" placeholder="" name='productPrice' aria-label="Workers Available" value={product.productPrice} onChange={handleChange}/>
-                </div>
-              </div>
-              <div class="row mt-3">
-                <div class="col">
-                  <label htmlFor="target" class="form-label" >Audit Date</label>
-                  <input type="date" class="form-control" placeholder="" name="auditDate" aria-label="Stock For the Day" onChange={handleChange} required/>
-                </div>
-                <div class="col">
-                  <label htmlFor="target" class="form-label" >Target Date</label>
-                  <input type="date" name='targetDate' class="form-control" placeholder="" aria-label="Left Out Stock"onChange={handleChange} required/>
+                <div className="">
+                  <label htmlFor="target" className="form-label" >Product Price</label>
+                  <input type="number" className="form-control" placeholder="" name='productPrice' aria-label="Workers Available" value={product.productPrice} onChange={handleChange}/>
                 </div>
               </div>
-              <div class="row mt-3">
-                <div class="col">
-                  <label htmlFor="target" class="form-label" >Total Stock</label>
-                  <input type="number" name='targetStock' class="form-control" placeholder="" aria-label="Faulty Stock" value={product.stock?product.stock:0} onChange={handleChange}/>
+              <div className="row mt-3">
+                <div className="col">
+                  <label htmlFor="target" className="form-label" >Audit Date</label>
+                  <input type="date" className="form-control" placeholder="" name="auditDate" aria-label="Stock For the Day" onChange={handleChange} required/>
                 </div>
-                <div class="col">
-                  <label htmlFor="target" class="form-label">Budget</label>
-                  <input type="number" name='budget' class="form-control" placeholder="" aria-label="Faulty Production" value={product.budget} onChange={handleChange}/>
+                <div className="col">
+                  <label htmlFor="target" className="form-label" >Target Date</label>
+                  <input type="date" name='targetDate' className="form-control" placeholder="" aria-label="Left Out Stock"onChange={handleChange} required/>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col">
+                  <label htmlFor="target" className="form-label" >Total Stock</label>
+                  <input type="number" name='targetStock' className="form-control" placeholder="" aria-label="Faulty Stock" value={product.stock?product.stock:0} onChange={handleChange}/>
+                </div>
+                <div className="col">
+                  <label htmlFor="target" className="form-label">Budget</label>
+                  <input type="number" name='budget' className="form-control" placeholder="" aria-label="Faulty Production" value={product.budget} onChange={handleChange}/>
                 </div>
               </div>
               <div className='col mt-3'>
                 <label htmlFor="production" className="form-label">Production</label>
                 <input type="number" className='form-control' name="production" id="production" value={product.production} onChange={handleChange}/>
               </div>
-              <div class="mt-3" >
-                <label htmlFor="target" class="form-label" >Product Details<span style={{color:"rgb(170, 170, 170)"}}>(Include Test Details Also)</span></label>
+              <div className="mt-3" >
+                <label htmlFor="target" className="form-label" >Product Details<span style={{color:"rgb(170, 170, 170)"}}>(Include Test Details Also)</span></label>
                 <textarea name="productDetails" id="productInfo" placeholder="" rows={10} value={product.productDetails} onChange={handleChange}></textarea>
               </div>
-              <button type="submit" class="mt-3 btn btn-sm btn-dark mb-3">Update</button>
+              <button type="submit" className="mt-3 btn btn-sm btn-dark mb-3">Update</button>
             </form>
     </div>
   )
--- src/components/NewProduct/NewProduct.jsx
+++ src/components/NewProduct/NewProduct.jsx
@@ -4,35 +4,35 @@ function NewProduct() {
   return (
     <div className="container productContainer mb-3" style={{marginTop:"5rem"}}>
         <h3 className='productTitle'>Add to production</h3>
-        <form method="POST" action="http://localhost:5000/products" className="needs-validation" enctype="multipart/form-data">
-              <div class="row">
-                <div class=" mb-3">
-                  <label htmlFor="target" class="form-label mt-3" >Product Name</label>
-                  <input type="text" class="form-control" id="target" placeholder="" aria-label="Target For the Day" name='productName' required/>
+        <form method="POST" action="http://localhost:5000/products" className="needs-validation" encType="multipart/form-data">
+              <div className="row">
+                <div className=" mb-3">
+                  <label htmlFor="target" className="form-label mt-3" >Product Name</label>
+                  <input type="text" className="form-control" id="target" placeholder="" aria-label="Target For the Day" name='productName' required/>
                 </div>
-                <div class="">
-                  <label htmlFor="target" class="form-label" >Product Price</label>
-                  <input type="number" class="form-control" placeholder="" name='productPrice' aria-label="Workers Available" required/>
-                </div>
-              </div>
-              <div class="row mt-3">
-                <div class="col">
-                  <label htmlFor="target" class="form-label" >Audit Date</label>
-                  <input type="date" class="form-control" placeholder="" name="auditDate" aria-label="Stock For the Day" required/>
-                </div>
-                <div class="col">
-                  <label htmlFor="target" class="form-label" >Target Date</label>
-                  <input type="date" name='targetDate' class="form-control" placeholder="" aria-label="Left Out Stock" required/>
+                <div className="">
+                  <label htmlFor="target" className="form-label" >Product Price</label>
+                  <input type="number" className="form-control" placeholder="" name='productPrice' aria-label="Workers Available" required/>
                 </div>
               </div>
-              <div class="row mt-3">
-                <div class="col">
-                  <label htmlFor="target" class="form-label" >Total Stock</label>
-                  <input type="number" name='targetStock' class="form-control" placeholder="" aria-label="Faulty Stock" required/>
+              <div className="row mt-3">
+                <div className="col">
+                  <label htmlFor="target" className="form-label" >Audit Date</label>
+                  <input type="date" className="form-control" placeholder="" name="auditDate" aria-label="Stock For the Day" required/>
                 </div>
-                <div class="col">
-                  <label htmlFor="target" class="form-label">Budget</label>
-                  <input type="number" name='budget' class="form-control" placeholder="" aria-label="Faulty Production" required/>
+                <div className="col">
+                  <label htmlFor="target" className="form-label" >Target Date</label>
+                  <input type="date" name='targetDate' className="form-control" placeholder="" aria-label="Left Out Stock" required/>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col">
+                  <label htmlFor="target" className="form-label" >Total Stock</label>
+                  <input type="number" name='targetStock' className="form-control" placeholder="" aria-label="Faulty Stock" required/>
+                </div>
+                <div className="col">
+                  <label htmlFor="target" className="form-label">Budget</label>
+                  <input type="number" name='budget' className="form-control" placeholder="" aria-label="Faulty Production" required/>
                 </div>
               </div>
               <div className='col mt-3'>
@@ -41,14 +41,14 @@ function NewProduct() {
               </div>
               <div className="col mt-3">
                 <label htmlFor="pdf" className="form-label">Upload WI <span style={{color:"rgb(170, 170, 170)"}}>(Work Instruction)</span></label>
-                <input type="file" name='wiUrl' class="form-control" placeholder="" aria-label="Faulty Production" accept='.pdf' required/>
+                <input type="file" name='wiUrl' className="form-control" placeholder="" aria-label="Faulty Production" accept='.pdf' required/>
                 <div>* once uploaded can not be changed or updated thereafter.</div>
               </div>
-              <div class="mt-3" >
-                <label htmlFor="target" class="form-label" >Product Details<span style={{color:"rgb(170, 170, 170)"}}>(Include Test Details Also)</span></label>
+              <div className="mt-3" >
+                <label htmlFor="target" className="form-label" >Product Details<span style={{color:"rgb(170, 170, 170)"}}>(Include Test Details Also)</span></label>
                 <textarea name="productDetails" id="productInfo" placeholder="" rows={10}></textarea>
               </div>
-              <button type="submit" class="mt-3 btn btn-sm btn-dark mb-3">Add</button>
+              <button type="submit" className="mt-3 btn btn-sm btn-dark mb-3">Add</button>
             </form>
     </div>
   )
